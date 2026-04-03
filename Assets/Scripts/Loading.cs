ff0610f3971724ef66fb8c909b82b2cf38e940a9
vlad <vlad1slav200508@yandex.ru>
2026-04-03 11:18:13 +0300

    update loading screen. Add real load logic

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -1,31 +1,53 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.InputSystem;
 
-// Метод визуализации слайдера, пока только прогрузка слайдера
+
 public class Loading: MonoBehaviour
 {
     public Slider slider;
+    public TMP_Text continueText;
+    public string NextScene = "MainMenu";
 
     void Start()
-    {
-        
-        StartCoroutine(AnimateSlider());
-        Debug.Log("Загрузка завершена!");
-    }
-
-    IEnumerator AnimateSlider()
-    {
-
+    {   
+        //скрытие текста, готовим слайдер
+        continueText.gameObject.SetActive(false);
+        slider.gameObject.SetActive(true);
         slider.value = 0f;
 
-        while (slider.value < 1f)
-        {
-            slider.value += 0.0005f;
-            yield return null; 
-        }
-
-        
+        // запуск генератора
+        StartCoroutine(LoadSceneAsync());
     }
 
+    // Генератор, если оформить while в Start() игре пиздец. Unity пытается сделать все в 1 кадре, генератор стопит программу пока не выполнится кусок
+    IEnumerator LoadSceneAsync()
+    {
+        // Прогружаем сцену
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(NextScene);
+        asyncLoad.allowSceneActivation = false;
+        
+        while (asyncLoad.progress < 0.9f)
+         {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            slider.value = progress;
+        
+            yield return null;
+         }
+
+        slider.value = 1f;
+        slider.gameObject.SetActive(false);
+        continueText.gameObject.SetActive(true);
+
+        yield return new WaitUntil(() => Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame);
+
+        asyncLoad.allowSceneActivation = true;
+
+        Debug.Log("Сцена загружена");
+
+    }
+        
 }
